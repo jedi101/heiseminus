9086d3021523301f56b323dea0181300c4b30bfd
jedi101 <mythos007@gmx.de>
2021-02-17 18:54:43 +0100

    experimenting with recursive lookup function

--- heiseminus.js
+++ heiseminus.js
@@ -4,9 +4,10 @@ console.log("HeiseMinus: is loaded.");
 * Search Definitions
 */
 const searches = [
-    {"childElement" : ".heiseplus-logo-small", "parentElement": "ARTICLE", "action": "hide"},
-    {"childElement" : ".stage--heiseplus",     "parentElement": "ASIDE",   "action": "hide"},
-    {"childElement" : ".heiseplus-logo",       "parentElement": "svg",     "action": "replaceImg"},
+    { "childElement": ".heiseplus-logo-small", "parentElement": "ARTICLE", "action": "hide" },
+    { "childElement": ".stage--heiseplus", "parentElement": "ASIDE", "action": "hide" },
+    { "childElement": ".heiseplus-logo", "parentElement": "svg", "action": "replaceImg" },
+    { "childElement": ".a-box__target", "parentElement": "DIV", "action": "hide" }
 ];
 
 /**
@@ -28,23 +29,24 @@ function hideHeisePlus(searches) {
     console.log("HeiseMinus: searching HeisePlus Articles.");
 
     // Iterate over the searches Object
-    searches.forEach( search => {
+    searches.forEach(search => {
         let stats = 0;
 
         // search the childElements
-        const selection = document.querySelectorAll (search.childElement);
+        const selection = document.querySelectorAll(search.childElement);
 
         // Iterate over all found childElements
-        selection.forEach( child => {
+        selection.forEach(child => {
 
             // search the parentElement
-            parent = findParent(child, search.parentElement);
-            
-            if (parent !== null) { 
+            // parent = findParent(child, search.parentElement);
+            parent = recursiveBacktraceToParentNode(child, search.parentElement);
+
+            if (parent !== null) {
                 // if parentElement is found then do the magic
                 stats++; // increase the stats
                 if (search.action == "hide") {
-                    parent.style.display = "none"; 
+                    parent.style.display = "none";
                 } else if (search.action == "replaceImg") {
                     console.log(child)
                     let new_logo = document.createElement("img")
@@ -63,23 +65,34 @@ function hideHeisePlus(searches) {
     })
 }
 
-/**
- * Find the parent of a the specified tag and work the way up to selct
- * his parent node and return it.
- * based on https://stackoverflow.com/a/7333885
- * @param {Node} el starting node for reverse parent look up operation
- * @param {String} tag specifier of the end nodes node name
- * 
- * @return {Node} return parent node or null if no parent node found
- */
-function findParent(el, tag) {
-    if (el.tagName === tag ) {
-        return el;
+// /* *
+//  * Find the parent of a the specified tag and work the way up to selct
+//  * his parent node and return it.
+//  * based on https://stackoverflow.com/a/7333885
+//  * @param {Node} startNodede starting node for reverse parent look up operation
+//  * @param {String} IdentifierTagName specifier of the end nodes node name
+//  * 
+//  * @return {Node} return parent node or null if no parent node found
+//  */
+// function findParent(startNode, IdentifierTagName) {
+//     if (startNode.tagName === IdentifierTagName) {
+//         return startNode;
+//     }
+//     while (startNode.parentNode) {
+//         startNode = startNode.parentNode;
+//         if (startNode.tagName === IdentifierTagName)
+//             return startNode;
+//     }
+//     return null;
+// }
+
+function recursiveBacktraceToParentNode(startNode, endNodeTagName) {
+    let currentNode = startNode
+
+    if (currentNode.tagName === endNodeTagName) {
+        return currentNode
     }
-    while (el.parentNode) {
-        el = el.parentNode;
-        if (el.tagName === tag)
-            return el;
-    }
-    return null;
+    
+    currentNode = currentNode.parentNode
+    recursiveBacktraceToParentNode(currentNode, endNodeTagName)
 }
