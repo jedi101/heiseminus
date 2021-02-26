dcfae89b501be2312d8366f0f1ec53b6bb1c1f13
jedi101 <mythos007@gmx.de>
2021-02-26 22:51:09 +0100

    trying loading of external config file from url

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 async function main() {
     // create local file url to access config.json of the extension
-    const url = chrome.extension.getURL("config.json")
-
+    // const url = chrome.extension.getURL("config.json")
+    // const url = chrome.extension.getURL("https://codeberg.org/dasistdaniel/PayWallBlocker/raw/branch/onlineblocklist/PWB_BLOCKLIST.json")
+    
+    const url = chrome.extension.getURL("https://codeberg.org/jedi/heiseminus/src/commit/72585cf6fa2c5b7a1230abfbef91aefcd03d134a/config.json")
     // create a config parser to access content from config.json
     const configParser = new ConfigParser(url)
 
